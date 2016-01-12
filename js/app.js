8a3f8666f07a8d1d2a4ab6131b9a5e1457684a17
Scott Mackie <scott@streetcontxt.com>
2016-01-11 21:50:25 -0500

    Updated button

--- js/app.js
+++ js/app.js
@@ -48,11 +48,23 @@ for (j = 0; j < genres.length; j++) {
                 var play_icon = document.createElement('div');
                 play_icon.classList.add('play-icon');
 
+                var play_icon_outer = document.createElement('div');
+                play_icon_outer.classList.add('play-icon-outer');
+
+                var play_icon_inner = document.createElement('div');
+                play_icon_inner.classList.add('play-icon-inner');
+
+                var play_icon_arrow = document.createElement('div');
+                play_icon_arrow.classList.add('play-icon-arrow');
+
                 var text = document.createTextNode(movies[i].name);
 
+                play_icon_inner.appendChild(play_icon_arrow);
+                play_icon_outer.appendChild(play_icon_inner);
+                play_icon.appendChild(play_icon_outer);
                 tile.appendChild(play_icon);
-                tile.appendChild(text);
                 tile.appendChild(overlay);
+                tile.appendChild(text);
                 row.appendChild(tile);
             }
         }
@@ -98,6 +110,8 @@ for (i = 0; i < hover_tiles.length; i++) {
     // Mouseover
     mouseover_delay(hover_tiles[i], function(elem){
         elem.classList.add('tile-scale');
+        elem.children[0].classList.add('play-icon-hover');
+
         // Shift left side
         for (current_node = elem.previousSibling ; current_node !== null ; current_node = current_node.previousSibling){
             current_node.classList.add('shiftLeft');
@@ -112,6 +126,8 @@ for (i = 0; i < hover_tiles.length; i++) {
     // Mouseout
     mouseout_delay(hover_tiles[i], function(elem){
         elem.classList.remove('tile-scale');
+        elem.children[0].classList.remove('play-icon-hover');
+
         // Shift left side
         for (current_node = elem.previousSibling ; current_node !== null ; current_node = current_node.previousSibling){
             current_node.classList.remove('shiftLeft');
