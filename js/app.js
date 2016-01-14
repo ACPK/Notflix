843c093b8050eb8aabbf76368414ca31c186d04a
Scott Mackie <scott@streetcontxt.com>
2016-01-13 19:07:15 -0500

    added basic photographs

--- js/app.js
+++ js/app.js
@@ -41,9 +41,15 @@ for (j = 0; j < genres.length; j++) {
 
                 var tile = document.createElement('div');
                 tile.classList.add('tile');
+                tile.style.backgroundImage = ("url(images/" + movies[i].image + ")");
+
+                var tile_overlay = document.createElement('div');
+                tile_overlay.classList.add('tile-overlay');
 
                 var inner_tile = document.createElement('div');
                 inner_tile.classList.add('inner-tile');
+                inner_tile.style.backgroundImage = ("url(images/" + movies[i].inner_image + ")");
+
 
                 var play_icon = document.createElement('div');
                 play_icon.classList.add('play-icon');
@@ -57,13 +63,31 @@ for (j = 0; j < genres.length; j++) {
                 var play_icon_arrow = document.createElement('div');
                 play_icon_arrow.classList.add('play-icon-arrow');
 
-                var text = document.createTextNode(movies[i].name);
+                var text_area = document.createElement('div');
+                text_area.classList.add('text-area');
+
+                var text_header_format = document.createElement('h5');
+                text_header_format.classList.add('text-header');
+
+                var text_header = document.createTextNode(movies[i].name);
+
+                var text_body = document.createTextNode(movies[i].description);
+
 
                 play_icon_inner.appendChild(play_icon_arrow);
                 play_icon_outer.appendChild(play_icon_inner);
                 play_icon.appendChild(play_icon_outer);
+
+
+                text_header_format.appendChild(text_header);
+                text_area.appendChild(text_header_format);
+
+                text_area.appendChild(text_body);
+
+
                 inner_tile.appendChild(play_icon);
-                inner_tile.appendChild(text);
+                inner_tile.appendChild(text_area);
+                tile.appendChild(tile_overlay);
                 tile.appendChild(inner_tile);
                 row.appendChild(tile);
             }
@@ -85,7 +109,7 @@ var current_node = null;
 var mouseover_delay = function (elem, callback) {
     var timeout = null;
     elem.addEventListener('mouseover', function() {
-        timeout = setTimeout(function() { callback(elem); }, 200);
+        timeout = setTimeout(function() { callback(elem); }, 250);
     });
 
     elem.addEventListener('mouseout', function() {
@@ -101,17 +125,17 @@ var mouseout_delay = function (elem, callback) {
     });
 
     elem.addEventListener('mouseout', function() {
-        timeout = setTimeout(function() { callback(elem); }, 200);
+        timeout = setTimeout(function() { callback(elem); }, 250);
     });
 };
 
 for (i = 0; i < hover_tiles.length; i++) {
     hover_tiles[i].addEventListener('mouseover', function() {
-        this.classList.add('outer-tile-hover');
+        this.children[0].classList.add('tile-overlay-hover');
     });
 
     hover_tiles[i].addEventListener('mouseout', function() {
-        this.classList.remove('outer-tile-hover');
+        this.children[0].classList.remove('tile-overlay-hover');
     });
 
 }
@@ -122,7 +146,7 @@ for (i = 0; i < hover_tiles.length; i++) {
     // Mouseover
     mouseover_delay(hover_tiles[i], function(elem){
         elem.classList.add('tile-scale');
-        elem.children[0].classList.add('inner-tile-hover');
+        elem.children[1].classList.add('inner-tile-hover');
 
         // Shift left side
         for (current_node = elem.previousSibling ; current_node !== null ; current_node = current_node.previousSibling){
@@ -138,7 +162,7 @@ for (i = 0; i < hover_tiles.length; i++) {
     // Mouseout
     mouseout_delay(hover_tiles[i], function(elem){
         elem.classList.remove('tile-scale');
-        elem.children[0].classList.remove('inner-tile-hover');
+        elem.children[1].classList.remove('inner-tile-hover');
 
         // Shift left side
         for (current_node = elem.previousSibling ; current_node !== null ; current_node = current_node.previousSibling){
--- js/movies.js
+++ js/movies.js
@@ -3,147 +3,192 @@ movies = [
         name: "The Martian1",
         year: 2013,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Martian2",
         year: 2014,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Martian3",
         year: 2015,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Martian4",
         year: 2013,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Martian5",
         year: 2014,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Martian6",
         year: 2015,
         genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian7",
-        year: 2013,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian8",
-        year: 2014,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian9",
-        year: 2015,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian10",
-        year: 2013,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian11",
-        year: 2014,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
-    },
-    {
-        name: "The Martian12",
-        year: 2015,
-        genre: "Science Fiction",
-        description: "Space!",
-        text: "This is some more text."
+        image: "themartian.jpg",
+        inner_image: "themartian2.jpg",
+        description: "This is some more text."
     },
     {
         name: "The Bourne Legacy1",
         year: 2010,
         genre: "Action",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "The Bourne Legacy2",
         year: 2011,
         genre: "Action",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "The Bourne Legacy3",
         year: 2012,
         genre: "Action",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "The Bourne Legacy4",
+        year: 2012,
+        genre: "Action",
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "The Bourne Legacy5",
+        year: 2012,
+        genre: "Action",
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "The Bourne Legacy6",
+        year: 2012,
+        genre: "Action",
+        image: "bournelegacy.jpg",
+        inner_image: "bournelegacy2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "Paul Blart Mall Cop1",
         year: 2012,
         genre: "Comedy",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "Paul Blart Mall Cop2",
         year: 2012,
         genre: "Comedy",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "Paul Blart Mall Cop3",
         year: 2012,
         genre: "Comedy",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "Paul Blart Mall Cop4",
+        year: 2012,
+        genre: "Comedy",
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "Paul Blart Mall Cop5",
+        year: 2012,
+        genre: "Comedy",
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "Paul Blart Mall Cop6",
+        year: 2012,
+        genre: "Comedy",
+        image: "paulblartmallcop.jpg",
+        inner_image: "paulblartmallcop2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "The Drop1",
         year: 2012,
         genre: "Crime",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "The Drop2",
         year: 2012,
         genre: "Crime",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
     },
     {
         name: "The Drop3",
         year: 2012,
         genre: "Crime",
-        description: "Excitement!",
-        text: "Lots of interesting text."
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
     },
+    {
+        name: "The Drop4",
+        year: 2012,
+        genre: "Crime",
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "The Drop5",
+        year: 2012,
+        genre: "Crime",
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
+    },
+    {
+        name: "The Drop6",
+        year: 2012,
+        genre: "Crime",
+        image: "thedrop.jpg",
+        inner_image: "thedrop2.jpg",
+        description: "Lots of interesting text."
+    }
 ];
